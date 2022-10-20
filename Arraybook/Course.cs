637433cf35f4f58fa549745dcefd06da355a7acf
AkshitaRahangdale <akshitarahangdale@1720gmail.com>
2022-10-20 11:36:33 +0530

    Add project files.

---
+++ Arraybook/Course.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arraybook
+{
+    public class Course
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Duration { get; set; }
+        public int Fees { get; set; }
+    }
+
+    public class Progrmm
+    {
+        static void Main(string[] args)
+        {
+            Course[] c1 = new Course[2]
+            {
+                new Course { Id = 12, Name = "Java", Duration = "one month", Fees = 5900},
+                new Course { Id = 123, Name = "C sharp", Duration = "two month", Fees = 4900 }
+            };
+
+            foreach (Course c in c1)
+            {
+                Console.WriteLine($"{c.Id}{c.Name}{c.Duration}{c.Fees}");
+            }
+
+        }
+    }
+
+}
---
+++ Arraybook/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace Arraybook
+//{
+
+//    public class Book
+//    {
+//        public int Id { get; set; }
+//        public string Name { get; set; }
+//        public string Publisher { get; set; }
+//        public int Price { get; set; }
+//    }
+
+//    public class Progrmm
+//    {
+//        static void Main(string[] args)
+//        {
+//            Book[] b1 = new Book[2]
+//            {
+//                new Book { Id = 12, Name = "Book1", Publisher = "S.James", Price = 890 },
+//                new Book { Id = 123, Name = "Book2", Publisher = "H.P", Price = 990 }
+
+//            };
+//            foreach (Book b in b1)
+//            {
+//                Console.WriteLine($"{b.Id}{b.Name}{b.Publisher}{b.Price}");
+//            }
+
+//        }
+//    }
+//}
